0bdccf16d409feeaeb15af97f9eedf3c0e984e9d
Tim Sullivan <timsullivan97@gmail.com>
2018-08-09 23:07:28 -0700

    updating comments

--- first_test_case/first_test_case.rb
+++ first_test_case/first_test_case.rb
@@ -6,12 +6,16 @@ require "selenium-webdriver"
 Selenium::WebDriver::Chrome.driver_path="/Users/tim_sullivan/Downloads/chromedriver"
 driver = Selenium::WebDriver.for :chrome
 
+# Google Chrome browser opens up and navigates to the link specified below
 driver.navigate.to "http://google.com"
 
 element = driver.find_element(:name, 'q')
 element.send_keys "hireme@abouttobeunemployed.com"
+
+# Targets the source and find <type="submit">
 element.submit
 
+# Returns the title of the plugin
 puts driver.title
 
 driver.quit
